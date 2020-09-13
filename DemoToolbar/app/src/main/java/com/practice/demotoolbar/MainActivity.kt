cb263a78327e13a3eae05a898966fc7ea41aba3a
pjs3065 <tmakdlfwotjd@naver.com>
2020-09-13 23:30:28 +0900

    툴바 구현

---
+++ DemoToolbar/app/src/main/java/com/practice/demotoolbar/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.practice.demotoolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_refresh -> {
+                Toast.makeText(this, "select refresh button",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
